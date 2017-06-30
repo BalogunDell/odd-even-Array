82ed200c28a1822d79ac1bc126b39c79784b03c6
Balogun Abiodun <delighteddell@gmail.com>
2017-06-30 16:12:29 +0100

    Updated code

--- odd_even_array.js
+++ odd_even_array.js
@@ -1,23 +1,18 @@
 function generateArrays(num) {
-  let generated_array = [];
-  let even = [];
-  let odd = [];
+   let generated_array = [] , even = [] , odd = [];
   
   if(num === 0 || !arguments || !Number(num)) return false;
   
-    
   for (let u = 1; u <= num; u++) {
     generated_array.push(u);
   }
  
   for(let v = 1; v < generated_array.length; v++) {
 
-    if(generated_array[v] % 2 === 0) {
-      even.push(generated_array[v]);
-    }else {
-      odd.push(generated_array[v]);
-      }
+    generated_array[v] % 2 === 0 ? even.push(generated_array[v]) : odd.push(generated_array[v]);
+    
     }  
+    
   console.log("Generated array: " + generated_array +" \n\nEven numbers: "  + even + "\n\nOdd numbers: " + odd);
-
+  
 }
